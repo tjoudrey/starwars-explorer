d3cbc4eaf5d636fd101a585a1ca97f86e9e16415
tyler <tylerjoudrey@gmail.com>
2019-10-09 18:17:08 -0300

    loading spinner added

--- src/app/views/planet/planet.module.ts
+++ src/app/views/planet/planet.module.ts
@@ -4,12 +4,14 @@ import { MatCardModule } from "@angular/material/card";
 import { PlanetRoutingModule } from "./planet-routing.module";
 import { PlanetComponent } from "./planet.component";
 import { PlanetCardComponent } from "../../components/planet-card/planet-card.component";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
   imports: [
     CommonModule,
     PlanetRoutingModule,
     MatCardModule,
+    MatProgressSpinnerModule,
   ],
   declarations: [
     PlanetComponent,
